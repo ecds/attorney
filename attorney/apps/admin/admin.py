ffc6b002fab17a5c20fb3427f227c70bda5dce6c
sarepal <sepalme@emory.edu>
2015-10-15 15:46:57 -0400

    unregister downtime and eultheme from admin

--- attorney/apps/admin/admin.py
+++ attorney/apps/admin/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from downtime.models import Period
+from eultheme.models import Banner
+
+admin.site.unregister(Period)
+admin.site.unregister(Banner)
 
 # Register your models here.
